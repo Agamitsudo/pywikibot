c3311b0ea9724919ae9502e091c536498bb31542
agamitsudo <agamitsudo@gmail.com>
2016-01-14 09:59:22 +0100

    add digram generation script

--- README.txt
+++ README.txt
@@ -1,4 +1,8 @@
 Here scripts based on pywikibot used on wikipedia, wikicommons, wikidata, wiktionary:
 
-* ogg_files_usage_on_wikt.py: this script creates a list of non used ogg files on related fr.wiktionary articles; .ogg files are stored on wikiCommons (January 2016).
+* ogg_files_usage_on_wikt.py: this script creates a list of non used ogg files on related fr.wiktionary articles; .ogg files are stored on wikiCommons (January 2016)
+** https://fr.wiktionary.org/w/index.php?title=Utilisateur:Beno%C3%AEt_Prieur/French_pronunciation&oldid=21007070
+
+* diagram_creationdate_wikt.py: generates a diagram representing number of creation of thesaurus in French on fr.wiktionary since 2004 (January 2016)
+** 
 
---
+++ diagram_creationdate_wikt.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# Usage of .ogg files on fr.wiktionary.py
+
+import sys
+import pywikibot
+
+sys.path.insert(0, "/home/benoit/Documents/pywikibot-core/core/pywikibot")
+
+import pagegenerators
+import re
+import math
+	     
+siteWiktionary = pywikibot.Site(u'fr', u'wiktionary')
+siteWiktionary.login()
+
+simpleName = u"Thésaurus en français"
+topCategoryName = u"Catégorie:" + simpleName
+
+new_dict = {}
+
+#
+# mainMethod
+#
+def mainMethod(topCategoryName):
+
+	category = pywikibot.Category(siteWiktionary, topCategoryName)
+	print "this category is analysed..." + topCategoryName
+	
+	for item in category.newest_pages():
+		
+		year = str(item.oldest_revision.timestamp.year)
+		month = str(item.oldest_revision.timestamp.month)
+		key =  year + "-" + month
+
+		title = item.title()
+
+		print title + " " + key
+
+		if new_dict.get(key) == None:
+			new_dict[key] = 1;
+		else:
+			new_dict[key] = new_dict[key] + 1;	
+	
+	#https://fr.wiktionary.org/wiki/Module:Diagramme
+	diagram = u"{{ #invoke:Diagramme | histogramme | largeur = 1000 | hauteur = 550 \n| groupe 1 = "
+	values = u""
+	legends = u""
+
+	for y in range(2004, 2016):
+		for m in range (1, 13):
+
+			k = str(y) + "-" + str(m)
+
+			if new_dict.get(k) == None:
+				values = values + "0"
+			else:
+				values = values + str(new_dict.get(k))
+			
+			if m == 1:			
+				legends = legends + str(y)			
+			
+			if m != 12 or y != 2015:
+				values = values + ":"
+				legends = legends + ":"
+
+	return diagram + values + u"\n| couleurs = green \n| légendes = " + legends + u"\n| noms = Nombre de " + simpleName + u" créés ce mois }}"
+#
+# Main
+#
+
+result = mainMethod(topCategoryName)
+page = pywikibot.Page(siteWiktionary, u"Utilisateur:Benoît Prieur/" + simpleName)
+page.text = result
+page.save("done")	
+
+
+
