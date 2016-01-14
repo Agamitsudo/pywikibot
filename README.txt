728e85d85f5e1dea2a5e3523a913932a112093f0
agamitsudo <agamitsudo@gmail.com>
2016-01-14 23:13:49 +0100

    + cumulative diagram

--- README.txt
+++ README.txt
@@ -3,6 +3,6 @@ Here scripts based on pywikibot used on wikipedia, wikicommons, wikidata, wiktio
 * ogg_files_usage_on_wikt.py: this script creates a list of non used ogg files on related fr.wiktionary articles; .ogg files are stored on wikiCommons (January 2016)
 ** https://fr.wiktionary.org/w/index.php?title=Utilisateur:Beno%C3%AEt_Prieur/French_pronunciation&oldid=21007070
 
-* diagram_creationdate_wikt.py: generates a diagram representing number of creation of thesaurus in French on fr.wiktionary since 2004 (January 2016)
-** https://fr.wiktionary.org/w/index.php?title=Utilisateur:Benoît_Prieur/Thésaurus_en_français&oldid=21017991
+* diagram_creationdate_wikt.py: generates two diagrams representing the number of creation of thesaurus in French on fr.wiktionary since 2004, cumulative or not (January 2016)
+** https://fr.wiktionary.org/w/index.php?title=Utilisateur:Beno%C3%AEt_Prieur/Th%C3%A9saurus_en_fran%C3%A7ais&oldid=21018848
 
--- diagram_creationdate_wikt.py
+++ diagram_creationdate_wikt.py
@@ -43,27 +43,41 @@ def mainMethod(topCategoryName):
 	
 	#https://fr.wiktionary.org/wiki/Module:Diagramme (documentation)
 	diagram = u"{{ #invoke:Diagramme | histogramme | largeur = 1000 | hauteur = 550 \n| groupe 1 = "
+	diagramCumulative = u"{{ #invoke:Diagramme | histogramme | largeur = 1000 | hauteur = 550 \n| groupe 1 = "
+	
 	values = u""
+	valuesCumulative = u""
+	sumCumulative = 0
 	legends = u""
 
-	for y in range(2004, 2016):
+	for y in range(2004, 2017):
 		for m in range (1, 13):
 
 			k = str(y) + "-" + str(m)
+			
+			if m > 1 and y == 2016:
+				sumCumulative = 0
 
 			if new_dict.get(k) == None:
 				values = values + "0"
+				valuesCumulative += str(sumCumulative)
 			else:
-				values = values + str(new_dict.get(k))
+				values += str(new_dict.get(k))
+				sumCumulative += new_dict.get(k)
+				valuesCumulative += str(sumCumulative)
 			
 			if m == 1:			
 				legends = legends + str(y)			
 			
-			if m != 12 or y != 2015:
-				values = values + ":"
-				legends = legends + ":"
+			if m != 12 or y != 2016:
+				values += ":"
+				valuesCumulative += ":"
+				legends += ":"
 
-	return diagram + values + u"\n| couleurs = green \n| légendes = " + legends + u"\n| noms = Nombre de " + simpleName + u" créés ce mois }}"
+	diags = diagram + values + u"\n| couleurs = green \n| légendes = " + legends + u"\n| noms = Nombre de " + simpleName + u" créés ce mois }}"
+	diags += "\n\n"
+	diags += diagramCumulative + valuesCumulative + u"\n| couleurs = green \n| légendes = " + legends + u"\n| noms = Nombre cumulé de " + simpleName + "}}"
+	return diags
 #
 # Main
 #
