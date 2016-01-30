3cf1a3140e55a800d2eafe24304c523930017e31
agamitsudo <agamitsudo@gmail.com>
2016-01-30 19:36:34 +0100

    add diagram_size_time.py

--- README.txt
+++ README.txt
@@ -4,7 +4,10 @@ Here scripts based on pywikibot used on wikipedia, wikicommons, wikidata, wiktio
 ** https://fr.wiktionary.org/w/index.php?title=Utilisateur:Beno%C3%AEt_Prieur/French_pronunciation (see history)
 
 * diagram_creationdate_wikt.py: generates two diagrams representing the number of creation of thesaurus in French on fr.wiktionary since 2004, cumulative or not (January 2016)
-** https://fr.wiktionary.org/w/index.php?title=Utilisateur:Beno%C3%AEt_Prieur/Th%C3%A9saurus_en_fran%C3%A7ais  (see history)
+** https://fr.wiktionary.org/w/index.php?title=Utilisateur:Beno%C3%AEt_Prieur/Th%C3%A9saurus_en_fran%C3%A7ais (see history)
 
 * wikt_br_words_not_existing_on_fr: br words existing on br.wiktionary and not on fr.wiktionary  (January 2016)
-** https://fr.wiktionary.org/wiki/Utilisateur:Beno%C3%AEt_Prieur/Comparaison_fr_br
+** https://fr.wiktionary.org/wiki/Utilisateur:Beno%C3%AEt_Prieur/Comparaison_fr_br (see history)
+
+* diagram_size_time.py: global size (in octets) of thesaurus in French on fr.wiktionary, month by month since 2004  (January 2016)
+** https://fr.wiktionary.org/wiki/Utilisateur:Beno%C3%AEt_Prieur/SizeTime_Th%C3%A9saurus_en_fran%C3%A7ais (see history) 
---
+++ diagram_size_time.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+# Diagrams for thesaurus fr.wiktionary
+
+import sys
+
+sys.path.insert(0, "/home/benoit/Documents/pywikibot-core/core")
+import pywikibot
+
+sys.path.insert(0, "/home/benoit/Documents/pywikibot-core/core/pywikibot")
+import pagegenerators
+import re
+import math
+import time
+	     
+siteWiktionary = pywikibot.Site(u'fr', u'wiktionary')
+siteWiktionary.login()
+
+simpleName = u"Thésaurus en français"
+topCategoryName = u"Catégorie:" + simpleName
+
+new_dict = {}
+
+#
+# mainMethod
+#
+def mainMethod(topCategoryName):
+
+	category = pywikibot.Category(siteWiktionary, topCategoryName)
+	print "this category is analysed..." + topCategoryName
+	global_result_dict = {}
+
+	#
+	# ci-dessous on met à zéro le dictionnaire de "2004"-1 jusqu'à "2016-12"
+	#
+	for yg in range(2004, 2017):
+		for mg in range (1, 13):
+
+			kg = str(yg) + "-" + str(mg)
+			global_result_dict[kg] = 0
+
+	pages = pagegenerators.CategorizedPageGenerator(category, recurse=False)
+	for page in pages:
+		
+		print "this page is analysed..." + page.title()
+		local_dict = {}
+
+		#
+		# ci-dessous on met pour chaque mois durant lequel il y a eu au moins un édit, la valeur la plus récente
+		#
+		for info in page.fullVersionHistory(): # loops from the much recent to the much older
+			
+			size = len(info.text.encode('utf-8'))
+
+			key = str(info.timestamp.year)  + "-" + str(info.timestamp.month)
+
+			if local_dict.get(key) == None:
+				local_dict[key] = size;
+				print key + " :" + str(size)
+		
+		local_result_dict = {}
+		last = 0
+
+		#
+		# ci-dessous on met pour chaque mois la valeur de fin de mois ou celle du mois précédent
+		#
+		for y in range(2004, 2017):
+			for m in range (1, 13):
+
+				k = str(y) + "-" + str(m)
+
+				if y == 2004 and m == 1:
+
+					local_result_dict[k] = 0 # k == 2004-1 here
+					last = 0
+
+				elif local_dict.get(k) != None:
+
+					local_result_dict[k] = local_dict.get(k)
+					last = local_result_dict[k]
+
+				elif local_dict.get(k) == None: 
+
+					local_result_dict[k] = last	
+
+		for ygr in range(2004, 2017):
+			for mgr in range (1, 13):
+				kgr = str(ygr) + "-" + str(mgr)
+				global_result_dict[kgr] += local_result_dict[kgr]
+
+	values = u""
+	legends = u""
+
+	#
+	# ci-dessous on cumule les valeurs pour l'ensemble des thésaurus
+	#
+	for yf in range(2004, 2017):
+		for mf in range (1, 13):
+
+			# ici on "s'arrête" à janvier 2016 : à décaler selon la date de lancement de script
+			if yf == 2016 and mf > 1:
+				values += "0"
+			else:
+				kf = str(yf) + "-" + str(mf)
+				values += str(global_result_dict.get(kf))
+
+			if mf == 1:			
+				legends += str(yf)
+
+			if mf != 12 or yf != 2016:
+				values += ":"
+				legends += ":"
+
+	diagram = u"{{ #invoke:Diagramme | histogramme | largeur = 1000 | hauteur = 550 \n| groupe 1 = "
+			
+	diags = diagram + values + u"\n| couleurs = green \n| légendes = " + legends + u"\n| noms = L'évolution du poids global des thésaurus en français au fil du temps }}"
+	diags += "\n\n"
+
+	return diags
+
+#
+# Main
+#
+
+result = mainMethod(topCategoryName)
+page = pywikibot.Page(siteWiktionary, u"Utilisateur:Benoît Prieur/SizeTime_" + simpleName)
+page.text = result
+page.save("done")	
+
+
+
